7e42b2f02f4d86147d985b91aa5b2d7e2193cfb7
Muhammad Awais <m.awais.iqb@gmail.com>
2023-08-24 22:27:41 +0100

    fbsdk added for ios

--- ios/food_delivery/AppDelegate.mm
+++ ios/food_delivery/AppDelegate.mm
@@ -2,19 +2,38 @@
 #import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
 #import <GoogleMaps/GoogleMaps.h>
+#import <AuthenticationServices/AuthenticationServices.h>
+#import <SafariServices/SafariServices.h>
+#import <FBSDKCoreKit/FBSDKCoreKit-swift.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  //firebase
   [FIRApp configure];
+  //react-native-maps
   [GMSServices provideAPIKey:@"AIzaSyB4uOPrlEUJjx-tCcPm7BY5fn1gLwtB4BA"];
+  //facebook sdk
+  [[FBSDKApplicationDelegate sharedInstance] application:application
+                         didFinishLaunchingWithOptions:launchOptions];
+  
   self.moduleName = @"food_delivery";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+//new method for facebook sdk
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [[FBSDKApplicationDelegate sharedInstance]application:app
+                                                      openURL:url
+                                                      options:options];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
